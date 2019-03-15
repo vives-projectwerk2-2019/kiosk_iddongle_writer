f477b074dba22c532bfdf2766993c35ba6a506e6
Charlotte Pieters <charlotte@pietersdekeyser.net>
2019-03-15 18:15:16 +0100

    added mqtt

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://mqtt.labict.be')
+ 
+client.on('connect', function () {
+  client.subscribe('test/bug/id', function (err) {
+    if (!err) {
+      //client.publish('presence', 'Hello mqtt')
+      console.log("Successfully subscribed to test/bug/id")
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+})
