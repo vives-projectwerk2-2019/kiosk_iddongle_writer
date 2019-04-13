e2b949508d0cffdb5a423bd0f6ec5ecd2550688f
Charlotte-Pieters <charlotte.pieters@student.vives.be>
2019-04-13 09:47:41 +0000

    adjusting pi1 to ID from .env file

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const parseJson = require('parse-json');
 const i2c = require('i2c-bus')
+require('dotenv').config()
 var Validator = require('jsonschema').Validator;
 var v = new Validator();
 const EEPROM = 0x50; //7-bit slave address
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://mqtt.labict.be')
-var topic = 'test/bug/id'
+var client = mqtt.connect('mqtt://mqtt.labict.be')
+var topic = 'kiosk/'+process.env.ID +'/program-dongle'
+var topicStatus = 'kiosk/'+process.env.ID +'/status'
+var topicPing = 'kiosk/'+process.env.ID +'/ping'
+var busy = false;
+
 
 var schema = {
   "id": "/SchemaId",
   "type": "object",
   "properties": {
-    "id": {"type": "string"
-          }
+    "id": {
+      "type": "string"
+    }
   }
 };
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
+    if ((new Date().getTime() - start) > milliseconds) {
       break;
     }
   }
@@ -28,46 +34,57 @@ function sleep(milliseconds) {
 client.on('connect', function () {
   client.subscribe(topic, function (err) {
     if (!err) {
-      console.log("Successfully subscribed to test/bug/id")
+      console.log("Successfully subscribed to kiosk/"+process.env.ID +'/program-dongle')
     }
   })
 })
- 
+
 client.on('message', function (topic, message) {
-  try{
+  try {
+    busy = true;
+    client.publish(topicStatus, "{\"status\":\"busy\"}")
     var myObj = JSON.parse(message);
     v.addSchema(schema, '/SchemaId');
 
-    if(v.validate(myObj, schema).valid){
+    if (v.validate(myObj, schema).valid) {
       console.log("Object is validated!");
+      var id = myObj.id
+      const bufmessage = new Buffer.from(id, "hex")
+      console.log(bufmessage)
 
-      const bufmessage = new Buffer(myObj.id)
-      console.log(myObj.id)
-      //("00000000" + myObj.id).slice(-8)
-      
-      const buf = new Buffer([0x1F,0xE0])
+      const buf = new Buffer([0x1F, 0xE0])
       var buffer = new Buffer(10)
-      var arr = [buf, bufmessage];
-      buffer = Buffer.concat(arr);
+      var arr = [buf, bufmessage]
+      buffer = Buffer.concat(arr)
 
       const i2c1 = i2c.openSync(1)
-      sleep(1000);
-    
+      sleep(1000)
+
       i2c1.i2cWriteSync(EEPROM, 0x0A, buffer).toString()
-      sleep(2000);
+      sleep(2000)
 
       i2c1.closeSync();
       console.log("Done")
-      client.publish(topic,'done')
+      client.publish(topicStatus, "{\"status\":\"ack\"}")
+      busy = false
     }
-    else{
-      console.log("not validated");
+    else {
+      client.publish(topicStatus, "{\"status\":\"nack\"}")
+      console.log("not validated")
       console.log("try again")
+      busy = false
     }
   }
-  catch(err){
+  catch (err) {
+    client.publish(topicStatus, "{\"status\":\"nack\"}")
     console.log("error")
     console.log("try again")
+    busy = false
   }
-  
 })
+
+setInterval(() => {
+  if (busy == false) {
+    client.publish(topicPing, "{\"ping\":\"ping\"}") //+process.env.ID +
+  }
+}, 10000)
