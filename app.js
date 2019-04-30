1d229c10c2aa08765331979d06ea66dc4493abd1
Dieter Debou <dieter.debou@student.vives.be>
2019-04-30 15:43:02 +0200

    Final changes

--- app.js
+++ app.js
@@ -42,38 +42,54 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   try {
     busy = true;
-    client.publish(topicStatus, "{\"status\":\"busy\"}")
-    var myObj = JSON.parse(message);
-    v.addSchema(schema, '/SchemaId');
+    client.publish(topicStatus, "{\"status\":\"busy\"}", () => {
+      var myObj = JSON.parse(message);
+      v.addSchema(schema, '/SchemaId');
 
-    if (v.validate(myObj, schema).valid) {
-      console.log("Object is validated!");
-      var id = myObj.id
-      const bufmessage = new Buffer.from(id, "hex")
-      console.log(bufmessage)
+      if (v.validate(myObj, schema).valid) {
+        console.log("Object is validated!");
+        var id = myObj.id
+        const bufmessage = new Buffer.from(id, "hex")
+        console.log(bufmessage)
+        var buffertest = new Buffer(8)
+        const buf = new Buffer([0x00, 0x00])
+        var buffer = new Buffer(10)
+        var arr = [buf, bufmessage]
+        buffer = Buffer.concat(arr)
 
-      const buf = new Buffer([0x1F, 0xE0])
-      var buffer = new Buffer(10)
-      var arr = [buf, bufmessage]
-      buffer = Buffer.concat(arr)
+        const i2c1 = i2c.openSync(1)
+        sleep(1000)
+        i2c1.i2cWriteSync(EEPROM, 0x0A, buffer)
+        sleep(2000)
 
-      const i2c1 = i2c.openSync(1)
-      sleep(1000)
+        //i2c1.closeSync();
+        console.log("Done")
+        client.publish(topicStatus, "{\"status\":\"ack\"}")
+        busy = false
+        //const i2c1 = i2c.openSync(1)
+        //sleep(2000)
+        //i2c2.i2cWriteSync(EEPROM, 2, buf)
+        //sleep(2000)
+        console.log("buffertest: ")
+        console.log(buffertest)
+        i2c1.i2cWriteSync(EEPROM, 2, buf)
+        sleep(2000)
+        i2c1.i2cReadSync(EEPROM, 0X08, buffertest)
+        sleep(2000)
+        console.log("buffertest: ")
+        console.log(buffertest)
+        i2c1.closeSync();
+      }
+      else {
+        client.publish(topicStatus, "{\"status\":\"nack\"}")
+        console.log("not validated")
+        console.log("try again")
+        busy = false
+      }
 
-      i2c1.i2cWriteSync(EEPROM, 0x0A, buffer).toString()
-      sleep(2000)
+    })
 
-      i2c1.closeSync();
-      console.log("Done")
-      client.publish(topicStatus, "{\"status\":\"ack\"}")
-      busy = false
-    }
-    else {
-      client.publish(topicStatus, "{\"status\":\"nack\"}")
-      console.log("not validated")
-      console.log("try again")
-      busy = false
-    }
+
   }
   catch (err) {
     client.publish(topicStatus, "{\"status\":\"nack\"}")
